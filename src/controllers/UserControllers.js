fc5e252e2bee895ad89f4f3fe2b4fbf2ee39bfae
llammann <bd7n2v9tr.code.edu.az>
2023-12-22 03:37:26 +0400

    methods

---
+++ src/controllers/UserControllers.js
@@ -0,0 +1,57 @@
+const User = require("../models/UserModel");
+
+// GET ALL USER
+const getAllUsers = async (req, res) => {
+  let allUsers = await User.find({});
+  res.send(allUsers);
+
+  console.log("GET ALL USER");
+};
+
+// POST USER
+const postUser = async (req, res) => {
+  const newUser = new User(req.body);
+  newUser.save();
+
+  console.log("POST USER");
+};
+
+// DELETE USER
+const deleteUser = async (req, res) => {
+  let Userid = req.params.id;
+  let findUser = await User.findOne({ id: Userid });
+  let _id = findUser._id;
+  let deletedUser = await User.findByIdAndDelete(_id);
+  console.log("DELETE USER", deletedUser);
+};
+
+// GET USER BY ID
+const getUserById = async (req, res) => {
+  let Userid = req.params.id;
+  let findUser = await User.findOne({ id: Userid });
+  
+  res.send("GET USER BY ID", findUser);
+};
+
+// GET UPDATE USER
+const getUpdateUser = async (req, res) => {
+  let Userid = req.params.id;
+  let updatedUser = await User.findOneAndUpdate({ id: Userid }, req.body);
+  console.log("GET UPDATE USER", updatedUser);
+};
+
+// PUT USER
+const putUser = async (req, res) => {
+  let Userid = req.params.id;
+  let updatedUser = await User.replaceOne({ id: Userid }, req.body);
+  console.log("PUT USER", updatedUser);
+};
+
+module.exports = {
+  postUser,
+  getAllUsers,
+  deleteUser,
+  getUserById,
+  getUpdateUser,
+  putUser
+};
